d462743463681e9f2abc90c426b5fd30e0779725
Soni Kirtan Brijeshbhai <kirtan@Kirtan-Mac.local>
2023-05-18 00:47:36 +0530

    first commit

---
+++ Node_js/app.js
@@ -0,0 +1,3 @@
+const tut = require('./tut')
+console.log(tut);
+// console.log(tut(1,1));
---
+++ Node_js/tut.js
@@ -0,0 +1,11 @@
+const sum = (num1,num2) => num1+num2;
+const PI = 3.14;
+class somemath{
+    constructor()
+    {
+        console.log('Hi');
+    }
+}
+module.exports.sum = sum;
+module.exports.PI = PI;
+module.exports.somemath = somemath;
